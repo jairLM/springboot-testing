83deb2e8a72a267b3fa7b2ff9ea3eaa4e05df8ac
Luis Jair Lopez Murillo <brook_98@live.com>
2026-04-02 00:37:59 -0600

    Using lombok in employee class and we declare entity and columns values

---
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,29 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity //Entity declare the class as and entity jpa
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+
+
+}
